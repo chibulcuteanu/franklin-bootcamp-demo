d221d503c745ddcb32f5968e98138d4ca8d4cc2b
chibulcu <chibulcu@adobe.com>
2023-04-24 16:14:21 +0300

    Added image block js and css

--- blocks/image/image.js
+++ blocks/image/image.js
@@ -1,16 +1,16 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+  block.classList.add(`image-${cols.length}-cols`);
 
-  // setup image columns
+  // setup block image
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
+          // picture is only content in image
+          picWrapper.classList.add('image-img-col');
         }
       }
     });
